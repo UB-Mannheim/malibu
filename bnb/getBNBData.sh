837dec43e04fbee50dca6ffc05137f2f50e7a9e5
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2016-01-28 17:05:42 +0100

    Create data dir for bnb data unless it exists
    Signed-off-by: Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>

--- bnb/getBNBData.sh
+++ bnb/getBNBData.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cd /var/www/Tools/BNBDaten
+BNB_DATADIR="/var/www/Tools/BNBDaten"
+mkdir -pv "$BNB_DATADIR"
+cd "$BNBDaten"
 
 wget -r -np -l 1 -A zip http://www.bl.uk/bibliographic/bnbrdfxml.html
 
-for f in /var/www/Tools/BNBDaten/www.bl.uk/bibliographic/bnbrdf/*.zip
-do 
-unzip -o $f
-rm $f
+for f in $BNB_DATADIR/www.bl.uk/bibliographic/bnbrdf/*.zip
+do
+    unzip -o "$f"
+    rm "$f"
 done
