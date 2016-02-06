UB-Mannheim/malibu

commit 6d043b4901be4c8cf2586d3e19ce5ec1baff14a8

Fix string concatenation in PHP code
PhpStorm warns about a wrong operator.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

isbn/verkaufsinfo.php
@@ -25,7 +25,9 @@
  */
 function isbn10($z) {
     if (strlen($z) == 13) {
-        $t = (substr($z, 3, 1) + 2 * substr($z, 4, 1) + 3 * substr($z, 5, 1) + 4 * substr($z, 6, 1) + 5 * substr($z, 7, 1) + 6 * substr($z, 8, 1) + 7 * substr($z, 9, 1) + 8 * substr($z, 10, 1) + 9 * substr($z, 11, 1) ) % 11;
+        $t = (substr($z, 3, 1) . 2 * substr($z, 4, 1) . 3 * substr($z, 5, 1) . 4 * substr($z, 6, 1) .
+                5 * substr($z, 7, 1) . 6 * substr($z, 8, 1) . 7 * substr($z, 9, 1) . 8 * substr($z, 10, 1) .
+                9 * substr($z, 11, 1) ) % 11;
         if ($t == 10) {
             $t = 'X';
         }
@@ -38,7 +40,9 @@ function isbn10($z) {
 function isbn13($z) {
     if (strlen($z) == 10) {
         $z = '978' . substr($z, 0, 9);
-        $t = (10 - (( substr($z, 0, 1) + 3 * substr($z, 1, 1) + substr($z, 2, 1) + 3 * substr($z, 3, 1) + substr($z, 4, 1) + 3 * substr($z, 5, 1) + substr($z, 6, 1) + 3 * substr($z, 7, 1) + substr($z, 8, 1) + 3 * substr($z, 9, 1) + substr($z, 10, 1) + 3 * substr($z, 11, 1) ) % 10 )) % 10;
+        $t = (10 - ((substr($z, 0, 1) . 3 * substr($z, 1, 1) . substr($z, 2, 1) . 3 * substr($z, 3, 1) .
+                     substr($z, 4, 1) . 3 * substr($z, 5, 1) . substr($z, 6, 1) . 3 * substr($z, 7, 1) .
+                     substr($z, 8, 1) . 3 * substr($z, 9, 1) . substr($z, 10, 1) . 3 * substr($z, 11, 1)) % 10)) % 10;
         return $z . $t;
     } else {
         return $z;

isbn/b3kat.php
@@ -56,9 +56,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 $outputString = "<?xml version=\"1.0\"?>\n";

isbn/bl.php
@@ -49,9 +49,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 $outputString = "<?xml version=\"1.0\"?>\n";

isbn/gbv.php
@@ -55,9 +55,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 $outputString = "<?xml version=\"1.0\"?>\n";

isbn/hbz.php
@@ -55,9 +55,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 $outputString = "<?xml version=\"1.0\"?>\n";
@@ -68,9 +68,9 @@ for ($p = 1; $p <= yaz_hits($id); $p++) {
     $record = yaz_record($id, $p, "render;charset=iso5426,utf8");//render;charset=iso5426,utf8
     $error = yaz_error($id);
     if (!empty($error)) {
-        echo "Error Number: " + yaz_errno($id);
-        echo "Error Description: " + $error ;
-        echo "Additional Error Information: " + yaz_addinfo($id);
+        echo "Error Number: " . yaz_errno($id);
+        echo "Error Description: " . $error ;
+        echo "Additional Error Information: " . yaz_addinfo($id);
     }
     $recordArray =  explode("\x1e", $record);
     $header = substr($recordArray[0],0,24);

isbn/hebis.php
@@ -56,9 +56,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 

isbn/swb.php
@@ -53,9 +53,9 @@ if (isset($_GET['isbn'])) {
 yaz_wait();
 $error = yaz_error($id);
 if (!empty($error)) {
-    echo "Error Number: " + yaz_errno($id);
-    echo "Error Description: " + $error ;
-    echo "Additional Error Information: " + yaz_addinfo($id);
+    echo "Error Number: " . yaz_errno($id);
+    echo "Error Description: " . $error ;
+    echo "Additional Error Information: " . yaz_addinfo($id);
 }
 
 $outputString = "<?xml version=\"1.0\"?>\n";

commit 7a394eecf3369b16feadfd1adb9188ca5d0cb886

Fix ISBN calculation (issue #25)
The regression was introduced by
commit 6d043b4901be4c8cf2586d3e19ce5ec1baff14a8.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

isbn/verkaufsinfo.php
@@ -23,16 +23,17 @@
  * als Gesamtdarstellung wieder. Hierbei werden einige gängige
  * Anbieter dafür abgefragt.
  */
+
 function isbn10($z)
 {
     if (strlen($z) == 13) {
-        $t = (substr($z, 3, 1) . 2 * substr($z, 4, 1) . 3 * substr($z, 5, 1) . 4 * substr($z, 6, 1) .
-                5 * substr($z, 7, 1) . 6 * substr($z, 8, 1) . 7 * substr($z, 9, 1) . 8 * substr($z, 10, 1) .
-                9 * substr($z, 11, 1) ) % 11;
+        $z = substr($z, 3, 9);
+        $t = ($z[0] + 2 * $z[1] + 3 * $z[2] + 4 * $z[3] + 5 * $z[4] +
+              6 * $z[5] + 7 * $z[6] + 8 * $z[7] + 9 * $z[8]) % 11;
         if ($t == 10) {
             $t = 'X';
         }
-        return substr($z, 3, 9) . $t;
+        return $z . $t;
     } else {
         return $z;
     }
@@ -42,9 +43,9 @@ function isbn13($z)
 {
     if (strlen($z) == 10) {
         $z = '978' . substr($z, 0, 9);
-        $t = (10 - ((substr($z, 0, 1) . 3 * substr($z, 1, 1) . substr($z, 2, 1) . 3 * substr($z, 3, 1) .
-                     substr($z, 4, 1) . 3 * substr($z, 5, 1) . substr($z, 6, 1) . 3 * substr($z, 7, 1) .
-                     substr($z, 8, 1) . 3 * substr($z, 9, 1) . substr($z, 10, 1) . 3 * substr($z, 11, 1)) % 10)) % 10;
+        $t = (10 - (($z[0] + 3 * $z[1] + $z[2] + 3 * $z[3] +
+                     $z[4] + 3 * $z[5] + $z[6] + 3 * $z[7] +
+                     $z[8] + 3 * $z[9] + $z[10] + 3 * $z[11]) % 10)) % 10;
         return $z . $t;
     } else {
         return $z;