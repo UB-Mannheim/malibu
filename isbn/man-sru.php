e00fa92a09e73fe44e60f336ad204cba1236b1dc
Philipp Zumstein <philipp.zumstein@bib.uni-mannheim.de>
2021-08-02 18:13:06 +0200

    Normalisierung bei ISBN-Filterung verbessert
    Andere Trenner z.B. Abstand oder Punkte werden jetzt bei der Normalisierung
    der ISBN ebenfalls weggefiltert.

--- isbn/man-sru.php
+++ isbn/man-sru.php
@@ -80,13 +80,13 @@ $outputString .= "<collection>\n";
 $outputArray = [];
 
 foreach ($records as $record) {
-    // Filter out any other results which contain the ISBN but not in the 020 field
+    // Filter out any other results which contain the ISBN but not in the 020 or 776 field
     $foundMatch = false;
     $foundIsbns = $xpath->query('.//datafield[@tag="020" or @tag="776"]/subfield', $record);
     foreach ($foundIsbns as $foundNode) {
         $foundValue = $foundNode->nodeValue;
         foreach ($nArray as $queryValue) {
-            $testString = str_replace("-", "", $queryValue);
+            $testString = preg_replace('/[^0-9xX]/', '', $queryValue);
             if (strlen($testString) == 13) {
                 // Delete the 978-prefix and the check value at the end for ISBN13
                 $testString = substr($testString, 3, -1);
@@ -94,7 +94,7 @@ foreach ($records as $record) {
                 // Delete check value at the end for ISBN10
                 $testString = substr($testString, 0, -1);
             }
-            if (strpos(str_replace("-", "", $foundValue), $testString) !== false) {
+            if (strpos(preg_replace('[^0-9xX]', '', $foundValue), $testString) !== false) {
                 $foundMatch = true;
             }
         }
